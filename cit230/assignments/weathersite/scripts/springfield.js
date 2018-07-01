e56ea869f3c24b99e732c37bb3cf35d7e98c60bc
thedavidlaws <38739135+thedavidlaws@users.noreply.github.com>
2018-06-30 19:15:51 -0600

    weathersite
    Final project

---
+++ cit230/assignments/weathersite/scripts/springfield.js
@@ -0,0 +1,21 @@
+var weatherRequest = new XMLHttpRequest();
+
+
+weatherRequest.open('GET','https://api.openweathermap.org/data/2.5/weather?id=4250542&APPID=c17825a6385810571c4c079ba7539f4b&units=imperial', true);
+
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+var weatherInfo = JSON.parse(weatherRequest.responseText);
+console.log(weatherInfo);
+
+document.getElementById('city').innerHTML = weatherInfo.name;
+document.getElementById('current').innerHTML = weatherInfo.main.temp;
+document.getElementById('description').innerHTML = weatherInfo.weather["0"].description;
+document.getElementById('hightemp').innerHTML = weatherInfo.main.temp_max;
+document.getElementById('lowtemp').innerHTML = weatherInfo.main.temp_min;
+document.getElementById('windspeed').innerHTML = weatherInfo.wind.speed;
+
+var icon = 'https://openweathermap.org/img/w/' + weatherInfo.weather["0"].icon + '.png';
+document.getElementById('weatherimage').src= icon;
+}
---
+++ cit230/assignments/weathersite/scripts/towns.js
@@ -0,0 +1,64 @@
+var section = document.querySelector('section');
+
+var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+var request = new XMLHttpRequest();
+
+request.open('GET', requestURL);
+
+request.responseType = 'json';
+
+request.send();
+
+request.onload = function() {
+
+    var skyTowns = request.response;
+
+    showInfo(skyTowns);
+}
+
+
+function showInfo(jsonObj) {
+
+    var info = jsonObj['towns'];
+    info.splice(2,1)
+
+    for (var i = 0; i < info.length; i++) {
+
+
+
+        var myArticle = document.createElement('article');
+        var myH2 = document.createElement('h2');
+        var myH3 = document.createElement('h3');
+        var myPara1 = document.createElement('p');
+        var myPara2 = document.createElement('p');
+        var myPara3 = document.createElement('p');
+        var myPara4 = document.createElement('p');
+        var myList = document.createElement('ul');
+
+        myH2.textContent = info[i].name;
+        myH3.textContent = 'Motto: ' + info[i].motto;
+        myPara1.textContent = 'Year Founded: ' + info[i].yearFounded;
+        myPara2.textContent = 'Current Population: ' + info[i].currentPopulation;
+        myPara3.textContent = 'Average Rainfall: ' + info[i].averageRainfall;
+        myPara4.textContent = 'Events: ';
+
+        var townEvents = info[i].events;
+        for (var j = 0; j < townEvents.length; j++) {
+
+
+            var listItem = document.createElement('li');
+            listItem.textContent = townEvents[j];
+            myList.appendChild(listItem);
+        }
+
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myH3);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+        myArticle.appendChild(myList);
+
+        section.appendChild(myArticle);
+    }
+}
