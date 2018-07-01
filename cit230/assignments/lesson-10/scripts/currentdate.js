456868eeacc6deeb5093509d5a6e7e7a24999868
thedavidlaws <38739135+thedavidlaws@users.noreply.github.com>
2018-06-30 19:01:04 -0600

    Week 10
    Here's my week 10 progress.

---
+++ cit230/assignments/lesson-10/scripts/currentdate.js
@@ -0,0 +1,32 @@
+{
+var d = new Date();
+var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+var month = new Array(12);
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+
+var dayOfMonth = d.getDate();
+
+var year = d.getFullYear();
+
+var n = weekday[d.getDay()] + ", " + dayOfMonth + " " + month[d.getMonth()] + " " + year;
+document.getElementById("currentdate").innerHTML = n;
+}
