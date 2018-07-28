65c0ffa2bde7ebcd38faf8beaabfc30c1a99f716
thedavidlaws <38739135+thedavidlaws@users.noreply.github.com>
2018-07-27 22:55:38 -0600

    Upload
    free

---
+++ cit230/assignments/lesson-8/jquery.js
@@ -0,0 +1,13 @@
+/* Jquery Code */
+$(document).ready(function() {
+$(".toggle").click(function() {
+$('.links').slideToggle();
+});
+$(window).resize(function() {
+if ($(window).width() > 768) {
+$('.links').show();
+} else {
+$('.links').hide();
+}
+});
+});
